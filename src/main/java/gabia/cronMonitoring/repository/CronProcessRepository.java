a5c2f4bd55c9db32aa483c89cc9d6c9d081b03ba
Lucas(주영준) <jyj@gabia.com>
2021-07-28 14:58:58 +0900

    feat: Spring Data Jpa로 변경

--- src/main/java/gabia/cronMonitoring/repository/CronProcessRepository.java
+++ src/main/java/gabia/cronMonitoring/repository/CronProcessRepository.java
@@ -4,15 +4,20 @@ import gabia.cronMonitoring.entity.CronProcess;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CronProcessRepository {
+public interface CronProcessRepository extends JpaRepository<CronProcess, Long> {
 
     List<CronProcess> findAllByCronJob_Id(UUID cronJobId);
 
     Optional<CronProcess> findByPid(String pid);
 
     CronProcess save(CronProcess cronProcess);
+
+    Page<CronProcess> findAllByCronJob_Id(UUID cronJobId, Pageable pageable);
 
 }
